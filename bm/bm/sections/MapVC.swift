3d708aa19477c9ea482543440f40585b65f63994
Pavle Mijatovic <paul.mijatovic@gmail.com>
2018-07-21 15:02:53 +0200

    Map logic

--- bm/bm/sections/MapVC.swift
+++ bm/bm/sections/MapVC.swift
@@ -26,7 +26,6 @@ class MapVC: UIViewController {
     // Vars
     var vehicles = [Vehicle]() {
         didSet {
-            print(vehicles.count)
             if vehicles.count == 0 {
                 print("gotovo.....")
             }
@@ -44,16 +43,16 @@ class MapVC: UIViewController {
     private var statisticsRefreshTimer: Timer?
     private var selectedStation: Station? {
         didSet {
-            print("=========selectedStation OBJECT===========")
-            print("id: \(self.selectedStation!.id)")
-            print("title: \(self.selectedStation!.title ?? "")")
+//            print("=========selectedStation OBJECT===========")
+//            print("id: \(self.selectedStation!.id)")
+//            print("title: \(self.selectedStation!.title ?? "")")
         }
     }
     private var randomStation: Station? {
         didSet {
-            print("=========randomStation OBJECT===========")
-            print("id: \(String(describing: self.randomStation?.id))")
-            print("title: \(self.randomStation?.title ?? "")")
+//            print("=========randomStation OBJECT===========")
+//            print("id: \(String(describing: self.randomStation?.id))")
+//            print("title: \(self.randomStation?.title ?? "")")
         }
     }
     
@@ -61,7 +60,7 @@ class MapVC: UIViewController {
     private var createBtn: UIBarButtonItem {
         return navigationItem.rightBarButtonItem!
     }
-    private var isCreationOfAnotherVehicleOK: Bool {
+    private var isCreationOfAnotherVehiclePossible: Bool {
         return isMaxAllowedVehicleNumberReached(currentNumberOfVehicles: activeVehicles, maxAllowed: maxVehiclesAllowed)
     }
     // Constants
@@ -100,7 +99,7 @@ class MapVC: UIViewController {
     }
     
     private func setNabBarBtn() {
-        let createBarBtn = UIBarButtonItem(title: "Create", style: .plain, target: self, action: #selector(createAction))
+        let createBarBtn = UIBarButtonItem(title: "Create", style: .plain, target: self, action: #selector(createTravelAction))
         createBarBtn.isEnabled = false
         navigationItem.rightBarButtonItem = createBarBtn
     }
@@ -108,20 +107,15 @@ class MapVC: UIViewController {
     // MARK: - Actions
     @objc func refreshVehiclInfo(sender: Timer) {
         for v in vehicles {
-            print("\(v.name) - remaining stations \(v.route.count)")
-            print("========")
-            
+//            print("\(v.name) - remaining stations \(v.route.count)")
             if v.route.count == 0 {
                 print("gotovo")
                 if let index = vehicles.index(of: v) {
                     vehicles.remove(at: index)
                     mapView.remove(v.polyline)
                     mapView.removeAnnotation(v)
-
                 }
             } else {
-                
-
                 mapView.removeAnnotation(v)
                 v.route.remove(at: 0)
                 if let currentLocation2D = v.route.first {
@@ -129,7 +123,6 @@ class MapVC: UIViewController {
                     v.location = currentLocation
                 }
                 mapView.addAnnotation(v)
-                
             }
         }
     }
@@ -142,9 +135,7 @@ class MapVC: UIViewController {
 //        let locationPoint = sender.location(in: mapView)
 //        let pickedCoordinate = mapView.convert(locationPoint, toCoordinateFrom: mapView)
 //
-//        let img = UIImage(named: "busPin")
 //        let vehicle = Vehicle(id: "xxxxx", name: "bus", image: UIImage(named: "busPin")!, latitude: pickedCoordinate.latitude, longitude: pickedCoordinate.longitude)
-//
 ////        let vehicle = Vehicle(id: "123", name: "busplus", type: "bus", latitude: pickedCoordinate.latitude, longitude: pickedCoordinate.longitude)
 //
 //        if self.tappedLocation != nil {
@@ -155,40 +146,45 @@ class MapVC: UIViewController {
 //        mapView.addAnnotation(vehicle)
     }
     
-    @objc func createAction(sender: UIBarButtonItem) {
+    @objc func createTravelAction(sender: UIBarButtonItem) {
+        guard isCreationOfAnotherVehiclePossible else {
+            print("\(activeVehicles) is max the number")
+            return
+        }
+        
         createRoute { (isRouteCreated) in
             guard isRouteCreated else { return }
-            // TODO: start travel
-            // .....
         }
     }
+    
+    func showRoute() {
+        
+    }
 
-    // Helper Temp
-    func createRoute(completion: (Bool) -> ()) {
-        // Check if ok
-        guard isCreationOfAnotherVehicleOK else {
-            print("Max number reached.....")
+    // MARK: - Helper
+    func createRoute(completion: @escaping (Bool) -> ()) {
+        self.randomStation = getRandomStationExcludingSelected(forMapView: mapView, view: mapView.view(for: mapView.selectedAnnotations.first!)!)
+        let sourceLocation = CLLocation(latitude: selectedStation!.coordinate.latitude, longitude: selectedStation!.coordinate.longitude)
+        
+        guard self.randomStation != nil else {
             completion(false)
             return
         }
-
-        self.randomStation = getRandomStationExcludingSelected(forMapView: mapView, view:  mapView.view(for: mapView.selectedAnnotations.first!)!)
-        let sourceLocation = CLLocation(latitude: selectedStation!.coordinate.latitude, longitude: selectedStation!.coordinate.longitude)
+        
         let destinationLocation = CLLocation(latitude: randomStation!.coordinate.latitude, longitude: randomStation!.coordinate.longitude)
         
         getRouteAndCoordinatesFor(sourceLocation: sourceLocation, destinationLocation: destinationLocation, locationManager: self.locationManager) { (route, coordinates) in
+        
+            
             self.mapView.add(route.polyline)
             let shortCoordinates = coordinates.extractArrayElements(withStep: coordinates.count / 20)
-            print(shortCoordinates.count)
-            print(coordinates.count)
-            print("============")
             
             let vh = Vehicle(id: "00  \(self.vehicles.count + 1)", name: "Bus No: \(self.vehicles.count + 1)", image: UIImage(named: "busPin")!, latitude: sourceLocation.coordinate.latitude, longitude: sourceLocation.coordinate.longitude, route: shortCoordinates  as! [CLLocationCoordinate2D], polyline: route.polyline)
             self.vehicles.append(vh)
+            
+            completion(true)
         }
-        completion(true)
     }
-    
     
     func getRouteAndCoordinatesFor(sourceLocation: CLLocation, destinationLocation: CLLocation, locationManager: CLLocationManager, success: @escaping (MKRoute, [CLLocationCoordinate2D]) -> Void) {
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
@@ -203,9 +199,9 @@ class MapVC: UIViewController {
         request.requestsAlternateRoutes = false
         request.transportType = .automobile
         
-        let region = MKCoordinateRegionMakeWithDistance(sourceLocation.coordinate, 15000, 15000)
-        mapView.setRegion(region, animated: true)
-        mapView.delegate = self
+//        let region = MKCoordinateRegionMakeWithDistance(sourceLocation.coordinate, 15000, 15000)
+//        mapView.setRegion(region, animated: true)
+//        mapView.delegate = self
         let directions = MKDirections(request: request)
         
         directions.calculate { (response, error) in
@@ -266,8 +262,6 @@ extension MapVC: MKMapViewDelegate {
             
             self.randomStation = getRandomStationExcludingSelected(forMapView: self.mapView, view: view)
         }
-        
-//        logSelectedAnnotation(mapView: self.mapView, view: view, stations: self.stations)
     }
     
     func getRandomStationExcludingSelected(forMapView mapView: MKMapView, view: MKAnnotationView) -> Station? {

7241d408aab59bd3caab58b792715832861aba91
Pavle Mijatovic <paul.mijatovic@gmail.com>
2018-07-23 14:52:37 +0200

    Map logic

--- bm/bm/resources/AppDelegate.swift
+++ bm/bm/resources/AppDelegate.swift
@@ -14,8 +14,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+//        setRootViewController()
         setRealmDBConfig()
-        startRealmDBInstance()
         startRealmDBInstance()
        
         return true
